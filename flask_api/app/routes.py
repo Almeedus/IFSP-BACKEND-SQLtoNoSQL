32af7286ea2716dbcdb8158626e371153b18be22
Eduardo Almeida <almeedusa@gmail.com>
2024-08-07 18:08:10 -0300

    (UPDATING FILE): Creating converters and closing connection

--- flask_api/app/routes.py
+++ flask_api/app/routes.py
@@ -18,3 +18,28 @@ def convert():
     # Conectar ao MongoDB
     mongo_client = MongoClient('mongodb://localhost:27017/')
     mongo_db = mongo_client['minha_base_nosql']
+    
+    # Converter dados de usuários
+    cursor.execute('SELECT * FROM usuario')
+    usuarios = cursor.fetchall()
+    mongo_db['usuario'].insert_many(usuarios)
+    
+    # Converter dados de endereços
+    cursor.execute('SELECT * FROM endereco')
+    enderecos = cursor.fetchall()
+    mongo_db['endereco'].insert_many(enderecos)
+    
+    # Converter dados de telefones
+    cursor.execute('SELECT * FROM telefone')
+    telefones = cursor.fetchall()
+    mongo_db['telefone'].insert_many(telefones)
+    
+    # Fechar conexões
+    cursor.close()
+    mysql_conn.close()
+    mongo_client.close()
+    
+    return jsonify(message='Dados convertidos com sucesso!'), 200
+
+def init_app(app):
+    app.register_blueprint(bp)
